56016e10b43c3e448249a1acdb591412d8e99f9c
WJSony <13283817653@163.com>
2022-01-18 12:15:59 +0800

    Manager

---
+++ WJModule/Classes/WJModuleManager.h
@@ -0,0 +1,16 @@
+//
+//  WJModuleManager.h
+//  WJModule
+//
+//  Created by 王洁 on 2022/1/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WJModuleManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WJModule/Classes/WJModuleManager.m
@@ -0,0 +1,12 @@
+//
+//  WJModuleManager.m
+//  WJModule
+//
+//  Created by 王洁 on 2022/1/18.
+//
+
+#import "WJModuleManager.h"
+
+@implementation WJModuleManager
+
+@end
